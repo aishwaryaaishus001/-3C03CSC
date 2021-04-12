808e37bd76c44850b5b4899cda9a261b517d0a05
aishwaryaaishus001 <69378925+aishwaryaaishus001@users.noreply.github.com>
2021-04-12 09:15:42 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,37 +3,48 @@ void main()
 {
 	int c;
 	float a,b,r;
-	printf("CALCULATOR\n");
-	printf("**********\n");
-	printf("1. Addition\n");
-	printf("2. Subtraction\n");
-	printf("3. Mltiplicatiion\n");
-	printf("4. Division\n");
-	printf("\n");
-	printf("Enter your choice: ");
-	scanf("%d",&c);
-	printf("Enter the two numbers: ");
-	scanf("%f%f",&a,&b);
-	switch(c)
-	{
-		case 1:
-			r=a+b;
-			printf("%f + %f = %f",a,b,r);
-			break;
-		case 2:
-			r=a-b;
-			printf("%f - %f = %f",a,b,r);
-			break;
-		case 3:
-			r=a*b;
-			printf("%f x %f = %f",a,b,r);
-			break;
-		case 4:
-			r=a/b;
-			printf("%f / %f = %f",a,b,r);
-			break;
-                default:
-                        printf("Invalid choice!!!");			
-	}
-	printf("\n");
+	do
+	{	
+		printf("CALCULATOR\n");
+		printf("**********\n");
+		printf("1. Addition\n");
+		printf("2. Subtraction\n");
+		printf("3. Mltiplicatiion\n");
+		printf("4. Division\n");
+		printf("5. Exit");
+		printf("\n");
+		printf("Enter your choice: ");
+		scanf("%d",&c);
+		if(c>0 && c<5)
+		{	
+			printf("Enter the two numbers: ");
+			scanf("%f%f",&a,&b);
+		}	
+		switch(c)
+		{
+			case 1:
+				r=a+b;
+				printf("%f + %f = %f",a,b,r);
+				break;
+			case 2:
+				r=a-b;
+				printf("%f - %f = %f",a,b,r);
+				break;
+			case 3:
+				r=a*b;
+				printf("%f x %f = %f",a,b,r);
+				break;
+			case 4:
+				r=a/b;
+				printf("%f / %f = %f",a,b,r);
+				break;
+			case 5:
+				printf("GODD BYE!!!\n");
+				break;
+			default:
+				printf("Invalid choice!!!");			
+		}
+		printf("\n\n");
+	}while(c!=5)'	
+	printf("\n\n");
 }	
